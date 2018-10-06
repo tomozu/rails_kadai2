da8886cb49ad66c858116b914b704a3ea16b4cd0
tomoko kanamori <humpty.dumpty.wt@gmail.com>
2018-10-06 12:53:08 +0000

    投稿確認画面修正その２

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,10 +16,14 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
     if@blog.save
-    redirect_to blogs_path,notice:"ブログを作成しました！"
-  else
-    render'new'
+      redirect_to blogs_path,notice:"ブログを作成しました！"
+    else
+      render'new'
     end
+  end
+  
+  def show
+    @blog = Blog.find(params[:id])
   end
   
   def edit
@@ -31,23 +35,19 @@ class BlogsController < ApplicationController
     render :new if @blog.invalid?
   end
   
-  def show
-   @blog = Blog.find(params[:id])
-  end
-  
-  def destroy
-  @blog.destroy
-    redirect_to blogs_path, notice:"ブログを削除しました！"
-  end
-  
   def update
     @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
-     redirect_to blogs_path, notice:"ブログを編集しました！"
+      redirect_to blogs_path, notice:"ブログを編集しました！"
     else
-     render 'edit'
+      render 'edit'
     end
-  end  
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
   
   private
   
